706ef18b335894c73e58778223ac5e485e22105f
BabuHubnet <155096831+BabuHubnet@users.noreply.github.com>
2024-01-04 19:20:06 +0530

    Sp Changes

--- DBChanges/10_SpGetAuthor.sql
+++ DBChanges/10_SpGetAuthor.sql
@@ -1,25 +1,50 @@
 USE [RSS ]
 GO
 
-/****** Object:  StoredProcedure [dbo].[SpGetAuthor]    Script Date: 29-12-2023 11:48:36 ******/
+/****** Object:  StoredProcedure [dbo].[SpGetAuthor]    Script Date: 04-01-2024 19:19:04 ******/
 DROP PROCEDURE [dbo].[SpGetAuthor]
 GO
 
-/****** Object:  StoredProcedure [dbo].[SpGetAuthor]    Script Date: 29-12-2023 11:48:36 ******/
+/****** Object:  StoredProcedure [dbo].[SpGetAuthor]    Script Date: 04-01-2024 19:19:04 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-
+ 
 CREATE PROC [dbo].[SpGetAuthor]
+(
+@sortColumn varchar(100),
+@sortOrder varchar(5)
+)
 AS
-SELECT BookId, 
-Title,
-AuthorFirstName,
-AuthorLastName 
-FROM [dbo].[Books] WITH(NOLOCK) 
+DECLARE @SQL NVARCHAR(max) 
+if(@sortColumn is not null and @sortOrder is not null and len(@sortOrder) >=3)
+begin    
+DECLARE @sortColumnMap varchar(100)
+DECLARE @sortOrderMap varchar(5) 
+	SET @sortColumnMap = @sortColumn
+	SET @sortOrderMap = case when @sortOrder = 'asc' then 'asc' else 'desc' end
+	SET @SQL= N'SELECT
+	AuthorFirstName +'', ''+ AuthorLastName + ''. "'' + Title + ''" <i>''+ ContentTitle +''</i>, '' +  Publisher +'', '' + Convert(varchar,Year(PublicationDate)) + '', pp. '' + PageRange + ''.'' As [Values]
+	FROM [dbo].[Books] b WITH(NOLOCK) 
+	Inner join [dbo].[BooksContents] bc With(nolock) on bc.BookId = b.BookId 
+	Group by b.BookId,Publisher,AuthorLastName,AuthorFirstName,Title,PublicationDate,PageRange,ContentTitle
+	order by case  when '''+ @sortColumnMap +'''=''Author'' then AuthorLastName +'',''+ AuthorFirstName   
+		when '''+ @sortColumnMap +'''=''Title'' then Title end  '+ @sortOrderMap +''
+end
+else
+begin
+SET @SQL= N'SELECT
+	AuthorFirstName +'', ''+ AuthorLastName + ''. "'' + Title + ''" <i>''+ ContentTitle +''</i>, ''+  Publisher +'', '' + Convert(varchar,Year(PublicationDate)) + '', pp. '' + PageRange + ''.'' As [Values]
+	FROM [dbo].[Books] b WITH(NOLOCK) 
+	Inner join [dbo].[BooksContents] bc With(nolock) on bc.BookId = b.BookId 
+	Group by b.BookId,Publisher,AuthorLastName,AuthorFirstName,Title,PublicationDate,PageRange,ContentTitle
+	order by AuthorLastName +  AuthorFirstName  asc,Title asc'
+End
+print @SQL
+EXEC sp_executesql @SQL
 GO
 
 
--- DBChanges/8_SpGetPublisher.sql
+++ DBChanges/8_SpGetPublisher.sql
@@ -1,11 +1,11 @@
 USE [RSS ]
 GO
 
-/****** Object:  StoredProcedure [dbo].[SpGetPublisher]    Script Date: 29-12-2023 11:48:15 ******/
+/****** Object:  StoredProcedure [dbo].[SpGetPublisher]    Script Date: 04-01-2024 19:10:37 ******/
 DROP PROCEDURE [dbo].[SpGetPublisher]
 GO
 
-/****** Object:  StoredProcedure [dbo].[SpGetPublisher]    Script Date: 29-12-2023 11:48:15 ******/
+/****** Object:  StoredProcedure [dbo].[SpGetPublisher]    Script Date: 04-01-2024 19:10:37 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -13,15 +13,39 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 
-
 CREATE PROC [dbo].[SpGetPublisher]
+(
+@sortColumn varchar(100),
+@sortOrder varchar(5)
+)
 AS
-SELECT BookId, 
-Title,
-Publisher,
-AuthorFirstName,
-AuthorLastName 
-FROM [dbo].[Books] WITH(NOLOCK) 
+DECLARE @SQL NVARCHAR(max) 
+if(@sortColumn is not null and @sortOrder is not null and len(@sortOrder) >=3)
+begin    
+DECLARE @sortColumnMap varchar(100)
+DECLARE @sortOrderMap varchar(5) 
+	SET @sortColumnMap = @sortColumn
+	SET @sortOrderMap = case when @sortOrder = 'asc' then 'asc' else 'desc' end
+	SET @SQL= N'SELECT
+	AuthorFirstName +'', ''+ AuthorLastName + ''. "'' + Title + ''" <i>''+ ContentTitle +''</i>, '' +  Publisher +'', '' + Convert(varchar,Year(PublicationDate)) + '', pp. '' + PageRange + ''.'' As [Values]
+	FROM [dbo].[Books] b WITH(NOLOCK) 
+	Inner join [dbo].[BooksContents] bc With(nolock) on bc.BookId = b.BookId 
+	Group by b.BookId,Publisher,AuthorLastName,AuthorFirstName,Title,PublicationDate,PageRange,ContentTitle
+	order by case when '''+ @sortColumnMap +'''= ''Publisher'' then Publisher  
+		when '''+ @sortColumnMap +'''=''Author'' then AuthorLastName +'',''+ AuthorFirstName   
+		when '''+ @sortColumnMap +'''=''Title'' then Title end  '+ @sortOrderMap +''
+end
+else
+begin
+SET @SQL= N'SELECT
+	AuthorFirstName +'', ''+ AuthorLastName + ''. "'' + Title + ''" <i>''+ ContentTitle +''</i>, ''+  Publisher +'', '' + Convert(varchar,Year(PublicationDate)) + '', pp. '' + PageRange + ''.'' As [Values]
+	FROM [dbo].[Books] b WITH(NOLOCK) 
+	Inner join [dbo].[BooksContents] bc With(nolock) on bc.BookId = b.BookId 
+	Group by b.BookId,Publisher,AuthorLastName,AuthorFirstName,Title,PublicationDate,PageRange,ContentTitle
+	order by Publisher  asc, AuthorLastName +  AuthorFirstName  asc,Title asc'
+End
+print @SQL
+EXEC sp_executesql @SQL 
 GO
 
 
